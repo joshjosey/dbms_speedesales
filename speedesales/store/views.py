be7fd80e96475ba7c70cf4e6a05661ce00c76cc0
joshjosey <156711944+joshjosey@users.noreply.github.com>
2024-11-12 20:32:01 -0600

    Templated Index
    Converted the index page into a templated version

--- speedesales/store/views.py
+++ speedesales/store/views.py
@@ -5,3 +5,7 @@ from .models import Product
 def index(request):
     products = Product.objects.all()
     return render(request, 'pages/index.html', {'products':products})
+
+def tech(request):
+    products = Product.objects.all()
+    return render(request, 'pages/tech.html', {'products':products})
